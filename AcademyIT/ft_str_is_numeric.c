4110506478136c21ed96caf38f0935defc4033b4
Loredana <20396933+Loredana0@users.noreply.github.com>
2022-04-07 17:46:18 +0930

    restore

--- AcademyIT/ft_str_is_numeric.c
+++ AcademyIT/ft_str_is_numeric.c
@@ -1,45 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llo-surd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/03 10:20:41 by llo-surd          #+#    #+#             */
-/*   Updated: 2022/01/17 12:24:34 by llo-surd         ###   ########.fr       */
+/*   Created: 2021/12/03 10:21:08 by llo-surd          #+#    #+#             */
+/*   Updated: 2021/12/06 10:03:25 by llo-surd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-<<<<<<< HEAD
-#include <stdio.h>
-int main()
-{
-	prinft("Hello from 42!");
-}
-=======
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_numeric(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
-		{
+		if (str[i] < '0' || str[i] > '9')
+		{		
 			return (0);
 		}
 		i++;
 	}
 	return (1);
 }
+
 /*
 #include <stdio.h>
 int    main(void)
 {
-	int alpha;
-    char a[] = "AaZz";
-    alpha = ft_str_is_alpha(a);
-    printf("%d\n", alpha);
+	int numeric;
+    char a[] = "123";
+    numeric = ft_str_is_numeric(a);
+    printf("%d\n", numeric);
 }
 */
->>>>>>> parent of 3237c37 (Revert "Add files via upload")
--- AcademyIT/ft_str_is_printable.c
+++ AcademyIT/ft_str_is_printable.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llo-surd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/03 10:20:41 by llo-surd          #+#    #+#             */
-/*   Updated: 2022/01/17 12:24:34 by llo-surd         ###   ########.fr       */
+/*   Created: 2021/12/03 10:23:57 by llo-surd          #+#    #+#             */
+/*   Updated: 2021/12/06 09:39:50 by llo-surd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-<<<<<<< HEAD
-#include <stdio.h>
-int main()
-{
-	prinft("Hello from 42!");
-}
-=======
-int	ft_str_is_alpha(char *str)
+int	ft_str_is_printable(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+		if (str[i] < 32 || str[i] > 126)
 		{
 			return (0);
 		}
@@ -32,14 +25,14 @@ int	ft_str_is_alpha(char *str)
 	}
 	return (1);
 }
+
 /*
 #include <stdio.h>
-int    main(void)
+int	main()
 {
-	int alpha;
-    char a[] = "AaZz";
-    alpha = ft_str_is_alpha(a);
-    printf("%d\n", alpha);
+	int printable;
+	char a[] = "\n";
+	printable = ft_str_is_printable(a);
+	printf("%d\n", printable);
 }
 */
->>>>>>> parent of 3237c37 (Revert "Add files via upload")
